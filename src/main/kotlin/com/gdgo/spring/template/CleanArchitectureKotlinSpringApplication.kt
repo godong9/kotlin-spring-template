d6269bb238aee9c29891979cfbf84a909ba2faa1
gd <godong9@gmail.com>
2022-02-24 23:06:34 +0900

    Init spring kotlin project

---
+++ src/main/kotlin/com/gdgo/spring/template/CleanArchitectureKotlinSpringApplication.kt
@@ -0,0 +1,11 @@
+package com.gdgo.spring.template
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CleanArchitectureKotlinSpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<CleanArchitectureKotlinSpringApplication>(*args)
+}
---
+++ src/test/kotlin/com/gdgo/spring/template/CleanArchitectureKotlinSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gdgo.spring.template
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CleanArchitectureKotlinSpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
